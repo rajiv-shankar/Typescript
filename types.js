4ba36456e901525664ae12b76576bf6b453913a8
Rajiv Shankar <rajiv.shankar.email@gmail.com>
2018-10-11 19:06:55 -0400

    Revisions

--- types.js
+++ types.js
@@ -1,3 +1,16 @@
 var myString;
-myString = "Hello world";
-console.log(myString);
+var myNum;
+var myBool;
+var myVar; // can be anything
+var strArr1;
+var numArr1;
+var boolArr1;
+var strArr2;
+var numArr2;
+var boolArr2;
+var strNumTuple; // has to match this order exactly
+myString = "Hello world".slice(0, 5) + "!";
+myNum = 0xf00d;
+myBool = true;
+strNumTuple = ["Hello", 4];
+console.log(strNumTuple);
--- types.ts
+++ types.ts
@@ -1,5 +1,21 @@
 let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;  // can be anything
 
-myString = "Hello world";
+let strArr1: string[];
+let numArr1: number[];
+let boolArr1: boolean[];
 
-console.log(myString);
+let strArr2: Array<string>;
+let numArr2: Array<number>;
+let boolArr2: Array<boolean>;
+
+let strNumTuple: [string, number];  // has to match this order exactly
+
+myString = "Hello world".slice(0,5) + "!";
+myNum = 0xf00d;
+myBool = true;
+strNumTuple = ["Hello", 4]
+
+console.log(strNumTuple);
