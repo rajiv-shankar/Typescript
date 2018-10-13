d76c6bad9202b7a0fc768b25e7ad007a82e7fffc
Rajiv Shankar <rajiv.shankar.email@gmail.com>
2018-10-13 15:59:19 -0400

    Revisions

---
+++ interfaces.js
@@ -0,0 +1,9 @@
+/*
+function showTodo(todo:{title: string, text: string}) {  // param "todo" has type {title: string, text: string}
+  console.log(todo.title + ": " + todo.text);
+}
+
+let myTodo = {title: "Trash", text: "Take out the trash"};
+
+showTodo(myTodo);
+*/
