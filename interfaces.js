73e4847f4c317ffbea131819970121d7a6f53a98
Rajiv Shankar <rajiv.shankar.email@gmail.com>
2018-10-15 17:51:30 -0400

    Revisions

--- interfaces.js
+++ interfaces.js
@@ -7,3 +7,8 @@ let myTodo = {title: "Trash", text: "Take out the trash"};
 
 showTodo(myTodo);
 */
+function showTodo(todo) {
+    console.log(todo.title + ": " + todo.text);
+}
+var myTodo = { title: "Trash", text: "Take out the trash" };
+showTodo(myTodo);
--- interfaces.ts
+++ interfaces.ts
@@ -8,4 +8,18 @@ let myTodo = {title: "Trash", text: "Take out the trash"};
 showTodo(myTodo);
 */
 
+// interface: syntactical contract that an entity should conform to,
+// defines the syntax that an entity must adhere to - properties, methods, and events
 
+interface Todo{
+  title: string;
+  text: string;
+}
+
+function showTodo(todo: Todo) {
+  console.log(todo.title + ": " + todo.text);
+}
+
+let myTodo = {title: "Trash", text: "Take out the trash"};
+
+showTodo(myTodo);
